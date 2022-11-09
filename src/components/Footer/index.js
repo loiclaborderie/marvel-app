5c923b1a1dce2deecbd70ae68f552155e66b3d5e
Loic L <l.laborderie-boulou@tbs-education.org>
2022-11-09 17:43:29 +0100

    mise en page de la first page

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-container">
+        <p>Projet réalisé sous React, par moi-même, en fin d'année 2022</p>
+        <p>
+          Les icônes Wolverne, Iron-Man, SpiderMan ainsi que BatMan proviennent
+          de iconfnder.com
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
